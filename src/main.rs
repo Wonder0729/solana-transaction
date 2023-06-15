56f3e52ef89f83a5dbc6fff8298545ee3ed112b7
Wonder0729 <135030265+Wonder0729@users.noreply.github.com>
2023-06-16 00:58:37 +0900

    get latest block number & block

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use dotenv::dotenv;
-use solana_client::rpc_client::RpcClient;
+use solana_client::{rpc_client::RpcClient, rpc_config::RpcBlockConfig};
+use solana_transaction_status::{EncodedConfirmedBlock, UiTransactionEncoding};
+
+fn get_block(client: &RpcClient, block_num: u64) -> EncodedConfirmedBlock {
+    log::debug!("Getting block number: {}", block_num);
+
+    let config = RpcBlockConfig {
+        encoding: Some(UiTransactionEncoding::Base64),
+        max_supported_transaction_version: Some(0),
+        ..Default::default()
+    };
+    
+    let block = client.get_block_with_config(block_num, config);
+    let encoded_block: EncodedConfirmedBlock = block.unwrap().into();
+
+    encoded_block
+}
 
 fn main() {
     dotenv().ok();
@@ -11,4 +27,8 @@ fn main() {
     let client = RpcClient::new("https://api.devnet.solana.com");
     let solana_version = client.get_version().unwrap().solana_core;
     log::info!("Solana version: {}", &solana_version);
+
+    let latest_block_number = client.get_slot().unwrap();
+    let block = get_block(&client, latest_block_number);
+    log::info!("Transactions count: {}", block.transactions.len());
 }
