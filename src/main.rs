576453a913c74da140063e214218ba726bd1eea1
Wonder0729 <135030265+Wonder0729@users.noreply.github.com>
2023-06-16 00:37:41 +0900

    connected solana RPC server and get solana core version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use dotenv::dotenv;
+use solana_client::rpc_client::RpcClient;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    env_logger::init();
+
+    log::info!("Count solana transactions per second");
+
+    // let client = RpcClient::new("https://solana-api.projectserum.com");
+    let client = RpcClient::new("https://api.devnet.solana.com");
+    let solana_version = client.get_version().unwrap().solana_core;
+    log::info!("Solana version: {}", &solana_version);
 }
